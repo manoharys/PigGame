1ad0e6dfead7bfbe4f86970414a02cf8a8f5de24
manohar <manumanohar1527@gmail.com>
2020-02-02 06:11:08 -0800

    implemented currentScore and roll dice events

--- app.js
+++ app.js
@@ -2,11 +2,10 @@
 
 //final scores of the player one
 let finalScore_1 = document.getElementById('finalScore-0');
-//console.log(finalScore_1.innerHTML);
+
 
 //final score of the second player.
 let finalScore_2 = document.getElementById('finalScore-1');
-//console.log(finalScore_2.innerHTML);
 
 //New game button
 let newGame = document.getElementById('new-game');
@@ -21,14 +20,17 @@ let hold = document.getElementById('hold');
 let currentScore_1 = document.getElementById("currentScore-0");
 let currentScore_2 = document.getElementById("currentScore-1");
 
+//initialising the values....
+init();
+
 /*Array named 'score[]' to contain the scores of the player
   player one score in index 0 and second player score in index 1*/
 let score = [0, 0];
+let PlayerscurrentScore = 0;
+let activePlayer = 0;
 
-//Players current scores
-let  playerOneCurrent = currentScore_1.innerHTML;
-let playerOneCurrentScore = parseInt(playerOneCurrent,10);
-
+//making dice invisible before starting the game.
+document.getElementById('img').style.display='none';
 
 //Adding the event listener to the roll dice button..
 rollDice.addEventListener('click', function () {
@@ -36,15 +38,30 @@ rollDice.addEventListener('click', function () {
     let random = Math.floor(Math.random() * 6) + 1;
     document.getElementById('img').src = './images/dice-' + random + '.png';
     
+    //making dice visible
+    document.getElementById('img').style.display='inline-block';
     //updating the random value to the current score until the player gets Number 1
     if (random !== 1) {
         //update the score
-         playerOneCurrentScore += random;
-    
+        PlayerscurrentScore += random;
+        document.getElementById('currentScore-' + activePlayer).textContent = PlayerscurrentScore;
+        document.getElementById('active-' + activePlayer).style.display = 'inline-block';
     } else {
-        //make the current score to zero and checkout to the next player
+        //Next players turn
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        PlayerscurrentScore = 0;
+        currentScore_1.textContent = '0';
+        currentScore_2.textContent = '0';
+        document.getElementById('active-0').style.display = 'none';
+        document.getElementById('active-1').style.display = 'none';
+        document.getElementById('img').style.display='none';
     }
-
 })
 
-//
+//init function 
+function init() {
+    finalScore_1.innerHTML = 0;
+    finalScore_2.innerHTML = 0;
+    currentScore_1.innerHTML = 0;
+    currentScore_2.innerHTML = 0;
+}
